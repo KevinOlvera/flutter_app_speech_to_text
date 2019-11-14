fc714dd55bdbdc8461df18e5fdd9b14b653ff3c3
Kevin Olvera <kevin.jesus.olvera@gmail.com>
2019-11-14 00:14:21 -0600

    My previous app code

---
+++ android/app/src/main/kotlin/com/kevinolvera/flutter_app_speech_to_text/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kevinolvera.flutter_app_speech_to_text
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
